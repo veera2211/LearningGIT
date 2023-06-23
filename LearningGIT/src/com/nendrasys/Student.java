9f258db7f117af2be8469bf9c05b2b79946d2c9c
veera2211 <133650436+veera2211@users.noreply.github.com>
2023-06-23 12:38:53 +0530

    Add files via upload

---
+++ LearningGIT/src/com/nendrasys/Student.java
@@ -0,0 +1,15 @@
+
+package com.nendrasys;
+
+public class Student {
+    int id=01;
+    String name1="Akhil";
+    String name="Veera Narayana";
+
+    public static void main(String args[]) {
+        Student s1 = new Student();
+        System.out.println(s1.id);
+        System.out.println(s1.name);
+        System.out.println(s1.name1);
+    }
+}
---
+++ LearningGIT/src/com/nendrasys/Vehicle.java
@@ -0,0 +1,22 @@
+package com.nendrasys;
+
+import java.sql.SQLOutput;
+
+public class Vehicle {
+
+    private static String name;
+    private static int wheels;
+    private static String fuel;
+
+    public static void main(String[] arg) {
+        System.out.println(name);
+        System.out.println(wheels);
+        System.out.println(fuel);
+        System.out.println(type);
+    }
+
+
+    private static String type;
+
+
+}
