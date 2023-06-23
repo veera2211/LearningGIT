19775c41fc7db715010116f7f46a0b02473d5ded
meena4144 <meena4144@gmail.com>
2023-06-23 13:37:58 +0530

    added another print statement

--- LearningGIT/src/com/nendrasys/Student.java
+++ LearningGIT/src/com/nendrasys/Student.java
@@ -2,14 +2,19 @@
 package com.nendrasys;
 
 public class Student {
-    int id=01;
+     int id=01;
     String name1="Akhil";
     String name="Veera Narayana";
 
+
     public static void main(String args[]) {
-        Student s1 = new Student();
+      Student s1 = new Student();
         System.out.println(s1.id);
         System.out.println(s1.name);
         System.out.println(s1.name1);
+         String name3= s1.name1+" "+s1.name;
+        System.out.println(name3);
+
+
     }
 }
